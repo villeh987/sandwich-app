bbd42518970f8564c81a92dc5b9d1e6b67f5cac0
Tommi Brusila <tommi.brusila@tuni.fi>
2021-04-12 16:32:35 +0300

    Delete unnecessary files that were accidentally committed, modify gitignore and improve test data

--- backend/server-a/service/OrderService.js
+++ backend/server-a/service/OrderService.js
@@ -56,7 +56,38 @@ exports.getOrderById = function(orderId) {
 exports.getOrders = function() {
   return new Promise(function(resolve, reject) {
     var examples = {};
-    examples['application/json'] = "";
+    examples['application/json'] = [
+      {
+      "sandwichId" : 6,
+      "id" : 0,
+      "status" : "ordered"
+      },
+      {
+      "sandwichId" : 85,
+      "id" : 2,
+      "status" : "recieved"
+      },
+      {
+      "sandwichId" : 45,
+      "id" : 777,
+      "status" : "inQueue"
+      },
+      {
+      "sandwichId" : 92,
+      "id" : 12,
+      "status" : "ready"
+      },
+      {
+      "sandwichId" : 5,
+      "id" : 443,
+      "status" : "failed"
+      },
+      {
+      "sandwichId" : 65,
+      "id" : 434,
+      "status" : "wrongParam"
+      },
+    ];
     if (Object.keys(examples).length > 0) {
       resolve(examples[Object.keys(examples)[0]]);
     } else {
